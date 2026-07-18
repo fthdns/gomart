0489096f53e0c71d15b6f81177d940ed69f8a47a
fthdns <fatihdaffa12@gmail.com>
2026-07-18 12:56:26 +0700

    Add database configuration

--- app/server.go
+++ app/server.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -29,14 +30,22 @@ type DBConfig struct {
 	DBPassword string
 	DBName     string
 	DBPort     string
+	DBDriver   string
 }
 
 func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
 	fmt.Println("Welcome to " + appConfig.AppName)
 
 	var err error
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBPort)
-	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if dbConfig.DBDriver == "mysql" {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBName)
+		server.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	} else {
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBPort)
+		server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	}
 
 	if err != nil {
 		panic("Failed on connecting to the database server")
@@ -74,10 +83,11 @@ func Run() {
 	appConfig.AppPort = getEnv("APP_PORT", "9999")
 
 	dbConfig.DBHost = getEnv("DB_HOST", "localhost")
-	dbConfig.DBUser = getEnv("DB_USER", "postgres")
-	dbConfig.DBPassword = getEnv("DB_PASSWORD", "supervisor")
+	dbConfig.DBUser = getEnv("DB_USER", "root")
+	dbConfig.DBPassword = getEnv("DB_PASSWORD", "root")
 	dbConfig.DBName = getEnv("DB_NAME", "gomartdb1")
-	dbConfig.DBPort = getEnv("DP_PORT", "5432")
+	dbConfig.DBPort = getEnv("DB_PORT", "3306")
+	dbConfig.DBDriver = getEnv("DB_DRIVER", "mysql")
 
 	server.Initialize(appConfig, dbConfig)
 	server.Run(": " + appConfig.AppPort)
