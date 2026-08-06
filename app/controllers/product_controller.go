e42eef0ce4379273d7e121d4649c35d4733a68bc
fthdns <fatihdaffa12@gmail.com>
2026-08-06 23:03:14 +0700

    Add initial detail product view

--- app/controllers/product_controller.go
+++ app/controllers/product_controller.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/fthdns/gomart/app/models"
+	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 )
 
@@ -37,5 +38,28 @@ func (server *Server) Products(w http.ResponseWriter, r *http.Request) {
 	_ = render.HTML(w, http.StatusOK, "products", map[string]interface{}{
 		"products":   products,
 		"pagination": pagination,
+	})
+}
+
+func (server *Server) GetProductBySlug(w http.ResponseWriter, r *http.Request) {
+	render := render.New(render.Options{
+		Layout: "layout",
+	})
+
+	vars := mux.Vars(r)
+
+	if vars["slug"] == "" {
+		return
+	}
+
+	productModel := models.Product{}
+	product, err := productModel.FindBySlug(server.DB, vars["slug"])
+
+	if err != nil {
+		return
+	}
+
+	_ = render.HTML(w, http.StatusOK, "product", map[string]interface{}{
+		"product": product,
 	})
 }
