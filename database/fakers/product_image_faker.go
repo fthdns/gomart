373975bb99dc2beca53d1975b96f7feac879d522
fthdns <fatihdaffa12@gmail.com>
2026-08-08 23:25:07 +0700

    Improve detail product view

---
+++ database/fakers/product_image_faker.go
@@ -0,0 +1,33 @@
+package fakers
+
+import (
+	"log"
+	"time"
+
+	"github.com/bxcodec/faker/v4"
+	"github.com/fthdns/gomart/app/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+func ProductImageFaker(db *gorm.DB) *models.ProductImage {
+	product := ProductFaker(db)
+	err := db.Create(&product).Error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &models.ProductImage{
+		ID:         uuid.New().String(),
+		Product:    models.Product{},
+		ProductID:  product.ID,
+		Path:       faker.URL(),
+		ExtraLarge: faker.URL(),
+		Large:      faker.URL(),
+		Medium:     faker.URL(),
+		Small:      faker.URL(),
+		CreatedAt:  time.Time{},
+		UpdatedAt:  time.Time{},
+		DeletedAt:  gorm.DeletedAt{},
+	}
+}
